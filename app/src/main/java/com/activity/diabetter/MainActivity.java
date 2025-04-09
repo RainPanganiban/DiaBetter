62a4c7014ede1d6d4fe2d81b491dafb0e367b2b0
lansilog <lhance.17avendano@gmail.com>
2025-04-09 11:42:08 +0800

    Button Functions

--- app/src/main/java/com/activity/diabetter/MainActivity.java
+++ app/src/main/java/com/activity/diabetter/MainActivity.java
@@ -1,6 +1,9 @@
 package com.activity.diabetter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +16,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button LoginButton = findViewById(R.id.LoginButton);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainMenu.class));
+            }
+        });
         
     }
 }
---
+++ app/src/main/java/com/activity/diabetter/MainMenu.java
@@ -0,0 +1,39 @@
+package com.activity.diabetter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        Button buttonStatisticCheck = findViewById(R.id.buttonStatisticsCheck);
+
+        buttonStatisticCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), StatisticsMenu.class));
+            }
+        });
+
+        Button buttonAlarmMenu = findViewById(R.id.buttonAlarmMenu);
+
+        buttonAlarmMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AlarmMenu.class));
+            }
+        });
+    }
+}
