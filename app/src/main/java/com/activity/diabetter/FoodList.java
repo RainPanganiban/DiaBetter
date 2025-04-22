1e7e1b2848e59bfa6d503e7c034fbad7f9130b0f
jdf2005 <jaydieldfuriscal@gmail.com>
2025-04-23 00:51:17 +0800

    FoodList-Button

--- app/src/main/java/com/activity/diabetter/FoodList.java
+++ app/src/main/java/com/activity/diabetter/FoodList.java
@@ -1,14 +1,12 @@
 package com.activity.diabetter;
 
-
 import android.os.Bundle;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FoodList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // links to XML layout
+        setContentView(R.layout.activity_food_list); // make sure this XML exists
     }
 }
